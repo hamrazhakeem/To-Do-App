dc4b74d344e68a32c68c2c749b34463375772c08
Hamraz Hakeem <hamrazhakeem100@gmail.com>
2024-06-05 16:53:08 +0530

    Second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { ToDoWrapper } from './ToDoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoWrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const components = () => {
+  return (
+    <div>components</div>
+  )
+}
